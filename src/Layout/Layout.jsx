a65fdabdfc3518eae99d20bb4c4e5ae6e2113651
MD Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2023-06-07 14:55:52 +0600

    make layout and header and footer

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+
+const Layout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
+
+export default Layout;
