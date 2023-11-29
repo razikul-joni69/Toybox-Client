4dba1df4dcb3a5a7df874e9d4405a705d24ae6f6
MD. Razikul Islam Joni <razikuljoni@gmail.com>
2023-11-29 20:15:37 +0600

    comment unimpliment features buttons etc, that feature will impliment later

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -1,7 +1,7 @@
 const Discount = () => {
     return (
         <div>
-            <div className="relative bg-fixed overflow-hidden h-screen rounded-lg bg-cover bg-no-repeat p-12 text-center bg-[url('https://demo.ishithemes.com/opencart/OPC082/image/catalog/parallaxbackground.png')]">
+            <div className="relative bg-fixed overflow-hidden h-screen rounded-lg bg-cover bg-no-repeat p-12 text-center bg-[url('https://images.unsplash.com/photo-1619768470847-f7db55f5d72e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
                 <div className="absolute bg-[rgba(0, 0, 0, 0.6)] bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed">
                     <div className="flex h-full justify-center lg:justify-start items-center ">
                         <div className="text-white space-y-5 lg:ml-10">
--- src/pages/ToyDetails.jsx
+++ src/pages/ToyDetails.jsx
@@ -117,7 +117,7 @@ const ToyDetails = () => {
                             </h4>
                         </div>
 
-                        <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                        {/* <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
                             <div className="flex">
                                 <span className="mr-3">Color</span>
                                 <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
@@ -148,14 +148,14 @@ const ToyDetails = () => {
                                     </span>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="flex">
                             <span className="title-font font-medium text-2xl text-gray-900">
-                                ${toy.price}
+                                Price: ${toy.price}
                             </span>
-                            <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+                            {/* <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
                                 Add To Cart
-                            </button>
+                            </button> */}
                             <button
                                 onClick={handleFavorite}
                                 className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4"
